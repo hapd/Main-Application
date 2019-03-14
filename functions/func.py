accb5ad88763f580f5109cb1ba3a9e856c4514d5
dc430 <deepakc3010@gmail.com>
2019-03-14 16:45:53 +0530

    Sign Up Complete

--- functions/func.py
+++ functions/func.py
@@ -1,32 +1,73 @@
-import datetime
+import datetime, json, requests
 
-def validate(self, name, nurse, dob, pin, mB, fB, contact):
-    print(('%s, %s, %s, %s, %s') % (name, nurse, dob, pin,contact))
+def sendDataToAPI(self, data, url, method):
+    data = json.dumps(data)
+    headers = {'Authorization' : '', 'Accept' : 'application/json', 'Content-Type' : 'application/json'}
+    try:
+        r = requests.post(url+method, data=data, headers=headers)
+        print("Data from api:\n", r.text)
+        return json.loads(r.text)    
+    except:
+        print("Couldn't send to webhook")
+
+
+
+
+
+
+
+
+
+def validateSignUpForm(self, name, nurse, dob, pin, mB, fB, contact):
+    # Checking if any of the gender is selected
     if(mB.status == False and fB.status == False):
         self.gender = None
-    if(len(str(pin)) != 4):  
+
+    # Checking if the pin is not of 4 digits    
+    if(len(str(pin)) != 4 and str(pin) != ''):  
         self.pinE = 1  
         self.status = 0
+    elif(str(pin) != ''):
+        self.pinE = 200
+
+    # Universal error for empty field    
     if(str(pin) == 0 or str(name) == '' or str(dob) == '' or str(nurse) == '' or str(contact) == ''):
         self.status = 0
+
+    # Checking for the correct date format of date of birth    
     try:  
         if(str(dob) != ''):
             datetime.datetime.strptime(dob,'%d/%m/%Y')  
+            self.dobE = 200
         else:
             self.dobE = 2
     except ValueError:  
         self.dobE = 1
         self.status = 0
+
+    # Error for not choosing any gender    
     if(self.gender == None):  
         self.genderE = 2  
         self.status = 0
-    if(str(contact).isnumeric() == False):  
-        self.contactE = 3
+
+    # Error when contact number does not contain numeric values    
+    if(str(contact).isnumeric() == False and str(contact) != ''):  
+        self.contactE = 2
         self.status = 0  
+    # Error when contact number is incomplete
     elif(len(str(contact))!=10 and str(contact) != ''):  
         self.contactE = 1  
         self.status = 0
+    elif(str(contact) != '' and len(str(contact)) == 10):
+        self.contactE = 200    
 
-    print('In validate:', self.status)
+    # Success code for name
+    if(str(name) != '' and str(name).isnumeric() == False):
+        self.nameE = 200
+
+    # Success code for nurse
+    if(str(nurse) != '' and str(nurse).isnumeric() == False):
+        self.nurseE = 200
+
      
     
--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 '''
 MAJOR PROJECT 2019 - CSE D (203,221,246,249)
-7 - Standard library imports
-13 - Kivy imports
 '''
 
 # Standard library imports
@@ -20,13 +18,16 @@ from kivy.uix.textinput import TextInput
 from kivy.properties import StringProperty, ObjectProperty
 from kivy.config import Config
 from kivymd.theming import ThemeManager
-
-#Local package imports
-from functions.func import validate
+from kivymd.bottomsheet import MDListBottomSheet
 
 
-#Global Variables
+# Local package imports
+from functions.func import validateSignUpForm, sendDataToAPI
 
+# Global Variables
+url = 'https://jolly-rabbit-50.localtunnel.me'
+
+# Class for Launch Screen
 class Launch(Screen):
     def __init__(self, **kwargs):
         super(Launch, self).__init__(**kwargs)
@@ -35,29 +36,59 @@ class Launch(Screen):
         self.quote = str(data["quotes"][str(random.randint(1,5))])
    
 
+# Class for Sign Up Screen
 class SignUp(Screen):
     def __init__(self, **kwargs):
         super(SignUp, self).__init__(**kwargs)
         self.nurseE = self.nameE = self.pinE = self.dobE = self.genderE = self.contactE = 0
         self.status = 1
         self.gender = None
+        self.pid = 'Nil'
     
     def sendFormData(self, name, nurse, dob, pin, mB, fB, contact):
         self.status = 1
-        validate(self, name, nurse, dob, pin, mB, fB, contact)
-        print('In sendFormData:', self.status)
+        validateSignUpForm(self, name, nurse, dob, pin, mB, fB, contact)
+        data = {
+            "name": name, 
+            "nurse": nurse,
+            "gender": self.gender, 
+            "dob": dob, 
+            "pin": pin,
+            "contact": contact 
+        }
+        if(self.status == 1):
+            r = sendDataToAPI(self, data, url, '/addUser')
+            self.pid = r['PID']
             
-
+        
+            
+# Class for Log In Screen
 class LogIn(Screen):
     pass
 
+# Class for the Screen Manager
 class Manager(ScreenManager):
     pass
 
+def callback():
+    Manager().current = 'Log In Screen'
+
+# Class for the main application
 class Main(App):
     theme_cls = ThemeManager()  # without this you'll get an error
     def build(self):
         return Builder.load_file('kv/main.kv')
+
+    
+    def show_example_bottom_sheet(self, pid):
+        bs = MDListBottomSheet()
+        bs.add_item("Your Patient ID is %s."%(pid), lambda x: x)
+        bs.add_item("Please note this down for future references.", lambda x: x,
+                    icon='information')
+        bs.add_item("Click on the back button to go to Log In Screen.", lambda x: x,
+                    icon='login')
+        bs.open()
+
 
 if __name__ == '__main__':
     Main().run()
